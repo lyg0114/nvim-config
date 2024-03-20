84f28d32cae5598687a388501c8e289a4db9d8f4
lyg0114 <whdnseowkd@naver.com>
2024-03-20 23:52:29 +0100

    init : neovim 설정파일

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,29 @@
+local keyMapper = require('utils.keyMapper').mapKey
+
+return {
+  {
+     "williamboman/mason.nvim",
+      config = function() 
+        require('mason').setup()
+      end
+  },
+  {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+        require('mason-lspconfig').setup({
+          ensure_installed = { "lua_ls", "tsserver", "html" }
+        })
+      end
+  },{
+      "neovim/nvim-lspconfig",
+      config = function()
+        local lspconfig = require('lspconfig')
+        lspconfig.lua_ls.setup({})
+        lspconfig.tsserver.setup({})
+
+        keyMapper('K', vim.lsp.buf.hover)
+        keyMapper('gd', vim.lsp.buf.definition)
+        keyMapper('<leader>ca', vim.lsp.buf.code_action)
+      end
+  }
+}
